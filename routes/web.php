eb4996508eb6d4b3636bb396c067de4a02d9e04b
P4ulaH <149234751+P4ulaH@users.noreply.github.com>
2025-08-16 23:25:19 +0200

    day 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('genres', GenreController::class);
+
