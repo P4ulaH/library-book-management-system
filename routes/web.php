1101a9e2c2090f8f6352705bf4732e0c98186bf5
P4ulaH <149234751+P4ulaH@users.noreply.github.com>
2025-11-22 20:34:48 +0100

    day 7 - clean folder and file structure

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,14 @@
 <?php
 
 use App\Models\Book;
-use App\Models\Author;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
+Route::get('/test', function () {
     $books = Book::with(['author','genre'])->get();
-    return view('index', compact('books'));
+    return view('testindex', compact('books'));
 });
 
 Route::resource('books', BookController::class);
